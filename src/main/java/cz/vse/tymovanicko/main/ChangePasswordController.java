d56a566d3850d2a46f7334a131b760e7acb786ae
kafj03 <kafj03@vse.cz>
2023-01-18 20:56:22 +0100

    vracení se zpět pomocí šipky u změny hesla

--- src/main/java/cz/vse/tymovanicko/main/ChangePasswordController.java
+++ src/main/java/cz/vse/tymovanicko/main/ChangePasswordController.java
@@ -2,7 +2,15 @@ package cz.vse.tymovanicko.main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * Třída  ChangePasswordController je hlavní třídou okna,
@@ -16,8 +24,35 @@ public class ChangePasswordController {
     // datové atributy
     @FXML
     private Button zmenHeslo;
+    @FXML
+    private ImageView zpet;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private void zpracujZmenuHesla(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void zpracujZpatky(MouseEvent mouseEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("profile_settings.fxml"));
+        stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void ztmavni(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        zpet.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlej(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        zpet.setEffect(zesvetleni);
     }
 }
