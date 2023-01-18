48eed9a2e5ebe63c2affdb8f7d63dea0036cd0de
kafj03 <kafj03@vse.cz>
2023-01-18 20:42:15 +0100

    najíždění na šipku

--- src/main/java/cz/vse/tymovanicko/main/RegisterController.java
+++ src/main/java/cz/vse/tymovanicko/main/RegisterController.java
@@ -3,6 +3,12 @@ package cz.vse.tymovanicko.main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.ColorInput;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 /**
  * Třída  RegisterController je hlavní třídou okna,
@@ -16,8 +22,24 @@ public class RegisterController {
     // datové atributy
     @FXML
     private Button zaregistruj;
+    @FXML
+    private ImageView zpet;
 
     @FXML
     private void zpracujZaregistrovani(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void ztmavni(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        zpet.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlej(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        zpet.setEffect(zesvetleni);
     }
 }
