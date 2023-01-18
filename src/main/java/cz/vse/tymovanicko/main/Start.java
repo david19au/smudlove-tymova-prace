a371d0e571c0dac5babd6f7fb57b2e67247fdd85
kafj03 <kafj03@vse.cz>
2023-01-18 15:52:22 +0100

    Start aplikace pomocí metody start

--- src/main/java/cz/vse/tymovanicko/main/Start.java
+++ src/main/java/cz/vse/tymovanicko/main/Start.java
@@ -1,7 +1,42 @@
 package cz.vse.tymovanicko.main;
 
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Třída  Start je hlavní třídou projektu,
+ * který představuje správu týmů
+ *
+ * @author    ?
+ * @version   ?
+ */
+public class Start extends Application {
+
+    /**
+     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     *
+     * @param args parametry příkazového řádku
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
+    }
+
+    /**
+     * Spouštění v okně
+     *
+     * @param stage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("login.fxml"));
+        loader.load();
+        Scene scene = new Scene(loader.getRoot());
+        stage.setScene(scene);
+        stage.setTitle("Týmováníčko");
+        stage.show();
     }
 }
