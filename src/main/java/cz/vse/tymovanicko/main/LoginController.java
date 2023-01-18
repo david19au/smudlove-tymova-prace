89f5374191500b19cd0b748a4b9d820e23c0e245
kafj03 <kafj03@vse.cz>
2023-01-18 16:19:46 +0100

    příprava login screenu

---
+++ src/main/java/cz/vse/tymovanicko/main/LoginController.java
@@ -0,0 +1,30 @@
+package cz.vse.tymovanicko.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+/**
+ * Třída  LoginController je hlavní třídou okna,
+ * který představuje přihlašovací obrazovku
+ *
+ * @author    ?
+ * @version   ?
+ */
+public class LoginController {
+
+    // datové atributy
+    @FXML
+    private Button prihlasit;
+    @FXML
+    private Button zaregistrovat;
+
+    @FXML
+    private void zpracujPrihlaseni() {
+
+    }
+
+    @FXML
+    private void zpracujZaregistrovani() {
+
+    }
+}
