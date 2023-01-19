4ab97beed72c7d571d00d3df91fd973cbb8aac71
kafj03 <kafj03@vse.cz>
2023-01-19 15:25:03 +0100

    funkční tlačítko nastavení

--- src/main/java/cz/vse/tymovanicko/main/HomeController.java
+++ src/main/java/cz/vse/tymovanicko/main/HomeController.java
@@ -1,9 +1,17 @@
 package cz.vse.tymovanicko.main;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HomeController {
 
@@ -14,6 +22,8 @@ public class HomeController {
     private ImageView chat;
     @FXML
     private ImageView nastaveni;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private void ztmavniKalendar(MouseEvent mouseEvent) {
@@ -55,5 +65,14 @@ public class HomeController {
         ColorAdjust zesvetleni = new ColorAdjust();
         zesvetleni.setBrightness(0);
         nastaveni.setEffect(zesvetleni);
+    }
+
+    @FXML
+    private void zpracujNaNastaveni(MouseEvent mouseEvent) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("profile_settings.fxml")));
+        stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
