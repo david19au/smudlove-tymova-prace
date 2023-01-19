477ed6195a7782b9476f37a970aac58a6833a807
kafj03 <kafj03@vse.cz>
2023-01-19 15:54:48 +0100

    tlačítka v chatu

--- src/main/java/cz/vse/tymovanicko/main/ChatController.java
+++ src/main/java/cz/vse/tymovanicko/main/ChatController.java
@@ -2,7 +2,18 @@ package cz.vse.tymovanicko.main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Třída  ChatController je hlavní třídou okna,
@@ -13,11 +24,55 @@ import javafx.scene.control.Button;
  */
 public class ChatController {
 
+
     // datové atributy
     @FXML
     private Button posli;
+    @FXML
+    private ImageView kalendar;
+    @FXML
+    private ImageView nastaveni;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private void zpracujPoslani(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void ztmavniKalendar(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        kalendar.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlejKalendar(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        kalendar.setEffect(zesvetleni);
+    }
+
+    @FXML
+    private void zpracujNaNastaveni(MouseEvent mouseEvent) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("profile_settings.fxml")));
+        stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void ztmavniNastaveni(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        nastaveni.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlejNastaveni(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        nastaveni.setEffect(zesvetleni);
     }
 }
