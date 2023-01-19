8876db5c4f6482f5b5399de12f760e3dc7e528c0
kafj03 <kafj03@vse.cz>
2023-01-19 15:13:38 +0100

    reakce tlačítek na myš

--- src/main/java/cz/vse/tymovanicko/main/ProfileSettingsController.java
+++ src/main/java/cz/vse/tymovanicko/main/ProfileSettingsController.java
@@ -3,6 +3,9 @@ package cz.vse.tymovanicko.main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Třída  ProfileSettingsController je hlavní třídou okna,
@@ -22,6 +25,10 @@ public class ProfileSettingsController {
     private Button odhlas;
     @FXML
     private Button uloz;
+    @FXML
+    private ImageView kalendar;
+    @FXML
+    private ImageView chat;
 
     @FXML
     private void zpracujUlozeni(ActionEvent actionEvent) {
@@ -37,5 +44,33 @@ public class ProfileSettingsController {
 
     @FXML
     private void zpracujNaZmenuTymu(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void ztmavniKalendar(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        kalendar.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlejKalendar(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        kalendar.setEffect(zesvetleni);
+    }
+
+    @FXML
+    private void ztmavniChat(MouseEvent mouseEvent) {
+        ColorAdjust ztmavnuti = new ColorAdjust();
+        ztmavnuti.setBrightness(-0.5);
+        chat.setEffect(ztmavnuti);
+    }
+
+    @FXML
+    private void zesvetlejChat(MouseEvent mouseEvent) {
+        ColorAdjust zesvetleni = new ColorAdjust();
+        zesvetleni.setBrightness(0);
+        chat.setEffect(zesvetleni);
     }
 }
