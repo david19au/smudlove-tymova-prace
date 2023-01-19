8e1c3a69cac5673c35e91c155c2f594a9917f182
kafj03 <kafj03@vse.cz>
2023-01-19 21:07:25 +0100

    ověřování emailu pomocí regexu

--- src/main/java/cz/vse/tymovanicko/main/RegisterController.java
+++ src/main/java/cz/vse/tymovanicko/main/RegisterController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.effect.ColorInput;
 import javafx.scene.image.ImageView;
@@ -17,6 +18,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Třída  RegisterController je hlavní třídou okna,
@@ -27,6 +30,7 @@ import java.util.Objects;
  */
 public class RegisterController {
 
+
     // datové atributy
     @FXML
     private Button zaregistruj;
@@ -34,9 +38,16 @@ public class RegisterController {
     private ImageView zpet;
     private Stage stage;
     private Scene scene;
+    @FXML
+    private TextField email;
 
     @FXML
     private void zpracujZaregistrovani(ActionEvent actionEvent) {
+        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern pattern = Pattern.compile(regex);
+        String string = email.getCharacters().toString();
+        Matcher matcher = pattern.matcher(string);
+        System.out.println(string + " : " + matcher.matches());
     }
 
     @FXML
