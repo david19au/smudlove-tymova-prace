6756fcbf563ad5a8475f1d4f6ef6c8ef0ae0314a
kafj03 <kafj03@vse.cz>
2023-01-19 15:21:17 +0100

    funkční tlačítko odhlásit se a změna hesla

--- src/main/java/cz/vse/tymovanicko/main/ProfileSettingsController.java
+++ src/main/java/cz/vse/tymovanicko/main/ProfileSettingsController.java
@@ -2,10 +2,18 @@ package cz.vse.tymovanicko.main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Třída  ProfileSettingsController je hlavní třídou okna,
@@ -29,17 +37,29 @@ public class ProfileSettingsController {
     private ImageView kalendar;
     @FXML
     private ImageView chat;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private void zpracujUlozeni(ActionEvent actionEvent) {
     }
 
     @FXML
-    private void zpracujOdhlaseni(ActionEvent actionEvent) {
+    private void zpracujOdhlaseni(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    private void zpracujNaZmenuHesla(ActionEvent actionEvent) {
+    private void zpracujNaZmenuHesla(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("change_password.fxml")));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
