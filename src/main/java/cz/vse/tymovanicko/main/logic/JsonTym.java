bf951b991c46f806b9036e4a3a39247410744c95
david19au <david19au@gmail.com>
2023-01-20 12:33:13 +0100

    přidání třídy týmu s kostrou datových prvků

---
+++ src/main/java/cz/vse/tymovanicko/main/logic/JsonTym.java
@@ -0,0 +1,29 @@
+package cz.vse.tymovanicko.main.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonTym {
+
+    private String jmenoTymu;
+    private String ucelTymu;
+    private List<JsonUzivatel> seznamClenu = new ArrayList<>();
+    private List<JsonUdalost> seznamUdalosti = new ArrayList<>();
+
+    public JsonTym(String jmenoTymu, String ucelTymu) {
+        this.jmenoTymu = jmenoTymu;
+        this.ucelTymu = ucelTymu;
+    }
+
+    public void pridejClena(JsonUzivatel clen) {
+        seznamClenu.add(clen);
+    }
+
+    public List<JsonUzivatel> getSeznamClenu () {
+        return seznamClenu;
+    }
+
+    public List<JsonUdalost> getSeznamUdalosti() {
+        return seznamUdalosti;
+    }
+}
