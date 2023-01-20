f6302612afa2f965772a03d6dafb8388ebecf339
kafj03 <kafj03@vse.cz>
2023-01-20 11:20:44 +0100

    ověřování údajů pomocí regexu

--- src/main/java/cz/vse/tymovanicko/main/RegisterController.java
+++ src/main/java/cz/vse/tymovanicko/main/RegisterController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.effect.ColorInput;
@@ -30,7 +31,6 @@ import java.util.regex.Pattern;
  */
 public class RegisterController {
 
-
     // datové atributy
     @FXML
     private Button zaregistruj;
@@ -40,14 +40,39 @@ public class RegisterController {
     private Scene scene;
     @FXML
     private TextField email;
+    @FXML
+    private TextField jmeno;
+    @FXML
+    private PasswordField heslo;
+    @FXML
+    private PasswordField potvrzeniHesla;
 
     @FXML
     private void zpracujZaregistrovani(ActionEvent actionEvent) {
-        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-        Pattern pattern = Pattern.compile(regex);
-        String string = email.getCharacters().toString();
-        Matcher matcher = pattern.matcher(string);
-        System.out.println(string + " : " + matcher.matches());
+        String regexJmeno = "^[A-ZŠČŘŽ][a-zěščřžýáíéúů]+(.[A-ZŠČŘŽ][a-zěščřžýáíéúů]+)+$";
+        Pattern patternJmeno = Pattern.compile(regexJmeno);
+        String stringJmeno = jmeno.getCharacters().toString();
+        Matcher jeValidniJmeno = patternJmeno.matcher(stringJmeno);
+
+        if (jeValidniJmeno.matches() == true) {
+            String regexEmail = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+            Pattern patternEmail = Pattern.compile(regexEmail);
+            String stringEmail = email.getCharacters().toString();
+            Matcher jeValidniEmail = patternEmail.matcher(stringEmail);
+
+            if (jeValidniEmail.matches() == true) {
+                String regexHeslo = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+                Pattern patternHeslo = Pattern.compile(regexHeslo);
+                String stringHeslo = heslo.getCharacters().toString();
+                Matcher jeValidniHeslo = patternHeslo.matcher(stringHeslo);
+
+                if (jeValidniHeslo.matches() == true) {
+                    if (stringHeslo.equals(potvrzeniHesla.getCharacters().toString())) {
+                        System.out.println("Vse je validni");
+                    } else System.out.println("Hesla se neshodují");
+                } else System.out.println("Není validní heslo");
+            } else System.out.println("Není validní email");
+        } else System.out.println("Není validní jméno");
     }
 
     @FXML
