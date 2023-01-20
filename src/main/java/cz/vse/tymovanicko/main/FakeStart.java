3d5ecf575dbc3676d880defd06c47cb26bde4ec9
david19au <david19au@gmail.com>
2023-01-20 12:54:01 +0100

    FakeStart aby Java neříkala že nemám JavaFX komponenty

---
+++ src/main/java/cz/vse/tymovanicko/main/FakeStart.java
@@ -0,0 +1,14 @@
+package cz.vse.tymovanicko.main;
+
+import java.io.IOException;
+
+public class FakeStart {
+    /**
+     * Metoda, která zavolá pravou třídu Main a předá jí případné args.
+     *
+     * @param args Parametry příkazového řádku / parametry spuštění, které se předají pravému Main
+     */
+    public static void main(String[] args) throws IOException {
+        Start.main(args);
+    }
+}
