574b446ff19288e4dbecaf8ad84a215999236ab6
david19au <david19au@gmail.com>
2023-01-22 13:03:12 +0100

    první kódy pro seznam událostí - not sure jestli fungují

---
+++ src/main/java/cz/vse/tymovanicko/logika/SeznamUdalosti.java
@@ -0,0 +1,22 @@
+package cz.vse.tymovanicko.logika;
+
+import java.util.ArrayList;
+
+public class SeznamUdalosti {
+
+    private ArrayList<Udalost> udalosti;
+
+    public SeznamUdalosti() {
+        udalosti = new ArrayList<>();
+    }
+
+    public void vlozUdalostiDoSeznamu(Udalost udalost) {
+        udalosti.add(udalost);
+    }
+
+public ArrayList<Udalost> getUdalosti() {
+    return udalosti;
+}
+
+
+}
