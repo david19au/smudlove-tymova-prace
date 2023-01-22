9adf410794c7df931be2109151c7b84cd42d3c3b
david19au <david19au@gmail.com>
2023-01-22 19:42:29 +0100

    kompletní překopání třídy Udalost.java

--- src/main/java/cz/vse/tymovanicko/logika/Udalost.java
+++ src/main/java/cz/vse/tymovanicko/logika/Udalost.java
@@ -1,101 +1,70 @@
 package cz.vse.tymovanicko.logika;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-/**
- * Třída Udalost - představuje udalost a její stav.
- *
- * Tato třída je součástí aplikace Týmováníčko.
- *
- * "Udalost" reprezentuje událost v aplikaci.
- *
- * @author     ?
- * @version    ?
- */
 public class Udalost {
+    private String jmenoUdalosti;
+    private String datumUdalosti;
+    private String lokaceUdalosti;
+    private List<String> seznamJde;
+    private List<String> seznamNejde;
+    private List<String> seznamNeodpovedeli;
 
-    private String nazev;
-    private String datumKonani;
-    private String mistoKonani;
-    private List<Uzivatel> seznamUcastnikuPrijdou;
-    private List<Uzivatel> seznamUcastnikuNeprijdou;
-    private List<Uzivatel> seznamUcastnikuNevi;
-
-    public Udalost(String nazev, String datumKonani, String mistoKonani, List<Uzivatel> seznamUcastnikuPrijdou, List<Uzivatel> seznamUcastnikuNeprijdou, List<Uzivatel> seznamUcastnikuNevi) {
-        this.nazev = nazev;
-        this.datumKonani = datumKonani;
-        this.mistoKonani = mistoKonani;
-        this.seznamUcastnikuPrijdou = new ArrayList<>();
-        this.seznamUcastnikuNeprijdou = new ArrayList<>();
-        this.seznamUcastnikuNevi = new ArrayList<>();
+    public Udalost(String jmenoUdalosti, Date datumUdalosti, String lokaceUdalosti) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        this.jmenoUdalosti = jmenoUdalosti;
+        this.datumUdalosti = sdf.format(datumUdalosti);
+        this.lokaceUdalosti = lokaceUdalosti;
     }
 
-    public void pridatPrijde(Uzivatel uzivatel) {
-        seznamUcastnikuPrijdou.add(uzivatel);
-        seznamUcastnikuNevi.remove(uzivatel);
+    public String getJmenoUdalosti() {
+        return jmenoUdalosti;
     }
 
-    public void pridatNeprijde(Uzivatel uzivatel) {
-        seznamUcastnikuNeprijdou.add(uzivatel);
-        seznamUcastnikuNevi.remove(uzivatel);
+    public void setJmenoUdalosti(String jmenoUdalosti) {
+        this.jmenoUdalosti = jmenoUdalosti;
     }
 
-    public void pridatNevi(Uzivatel uzivatel) {
-        seznamUcastnikuNevi.add(uzivatel);
+    public String getDatumUdalosti() {
+        return datumUdalosti;
     }
 
-    public void removeUser(Uzivatel uzivatel) {
-        seznamUcastnikuPrijdou.remove(uzivatel);
-        seznamUcastnikuNeprijdou.remove(uzivatel);
+    public void setDatumUdalosti(Date datumUdalosti) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        this.datumUdalosti = sdf.format(datumUdalosti);
     }
 
-    public String getNazev() {
-        return nazev;
+    public String getLokaceUdalosti() {
+        return lokaceUdalosti;
     }
 
-    public void setNazev(String nazev) {
-        this.nazev = nazev;
+    public void setLokaceUdalosti(String lokaceUdalosti) {
+        this.lokaceUdalosti = lokaceUdalosti;
     }
 
-    public String getDatumKonani() {
-        return datumKonani;
+    public List<String> getSeznamJde() {
+        return seznamJde;
     }
 
-    public void setDatumKonani(String datumKonani) {
-        this.datumKonani = datumKonani;
+    public void setSeznamJde(List<String> seznamJde) {
+        this.seznamJde = seznamJde;
     }
 
-    public String getMistoKonani() {
-        return mistoKonani;
+    public List<String> getSeznamNejde() {
+        return seznamNejde;
     }
 
-    public void setMistoKonani(String mistoKonani) {
-        this.mistoKonani = mistoKonani;
+    public void setSeznamNejde(List<String> seznamNejde) {
+        this.seznamNejde = seznamNejde;
     }
 
-    public List<Uzivatel> getSeznamUcastnikuPrijdou() {
-        return seznamUcastnikuPrijdou;
+    public List<String> getSeznamNeodpovedeli() {
+        return seznamNeodpovedeli;
     }
 
-    public void setSeznamUcastnikuPrijdou(List<Uzivatel> seznamUcastnikuPrijdou) {
-        this.seznamUcastnikuPrijdou = seznamUcastnikuPrijdou;
-    }
-
-    public List<Uzivatel> getSeznamUcastnikuNeprijdou() {
-        return seznamUcastnikuNeprijdou;
-    }
-
-    public void setSeznamUcastnikuNeprijdou(List<Uzivatel> seznamUcastnikuNeprijdou) {
-        this.seznamUcastnikuNeprijdou = seznamUcastnikuNeprijdou;
-    }
-
-    public List<Uzivatel> getSeznamUcastnikuNevi() {
-        return seznamUcastnikuNevi;
-    }
-
-    public void setSeznamUcastnikuNevi(List<Uzivatel> seznamUcastnikuNevi) {
-        this.seznamUcastnikuNevi = seznamUcastnikuNevi;
+    public void setSeznamNeodpovedeli(List<String> seznamNeodpovedeli) {
+        this.seznamNeodpovedeli = seznamNeodpovedeli;
     }
 }
-
