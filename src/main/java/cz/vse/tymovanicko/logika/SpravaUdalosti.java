0a6673f9a957df9c6a1dc57ee699b08e18b3ea81
david19au <david19au@gmail.com>
2023-01-23 23:24:52 +0100

    reformat kódu

--- src/main/java/cz/vse/tymovanicko/logika/SpravaUdalosti.java
+++ src/main/java/cz/vse/tymovanicko/logika/SpravaUdalosti.java
@@ -43,7 +43,8 @@ public class SpravaUdalosti {
                 try (Reader reader = new FileReader("target/udalosti.json")) {
                     JsonElement jsonElement = gson.fromJson(reader, JsonElement.class);
                     String jsonInString = gson.toJson(jsonElement);
-                    udalosti = gson.fromJson(jsonInString, new TypeToken<List<Udalost>>() {}.getType());
+                    udalosti = gson.fromJson(jsonInString, new TypeToken<List<Udalost>>() {
+                    }.getType());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -59,7 +60,8 @@ public class SpravaUdalosti {
         try (Reader reader = new FileReader("target/udalosti.json")) {
             JsonElement jsonElement = gson.fromJson(reader, JsonElement.class);
             String jsonInString = gson.toJson(jsonElement);
-            udalosti = gson.fromJson(jsonInString, new TypeToken<List<Udalost>>() {}.getType());
+            udalosti = gson.fromJson(jsonInString, new TypeToken<List<Udalost>>() {
+            }.getType());
         } catch (IOException e) {
             e.printStackTrace();
         }
