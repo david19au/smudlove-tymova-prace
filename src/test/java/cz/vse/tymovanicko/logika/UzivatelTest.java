a54cd715e7dd98b045341cc99c08412d5f9ede47
david19au <david19au@gmail.com>
2023-01-25 00:00:50 +0100

    komentáře test Uzivatel

--- src/test/java/cz/vse/tymovanicko/logika/UzivatelTest.java
+++ src/test/java/cz/vse/tymovanicko/logika/UzivatelTest.java
@@ -4,50 +4,82 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Metoda UzivatelTest kontroluje funkčnost metod této třídy
+ *
+ * @author ?
+ * @version 1.0.0
+ */
+
+
 public class UzivatelTest {
     Uzivatel u = new Uzivatel("user@email.com", "John", "Doe", "password");
 
+    /**
+     * Metoda testGetEmail kontroluje, že vrací správně email
+     */
     @Test
     public void testGetEmail() {
-        assertEquals("user@email.com", u.getEmail());
+        assertEquals("adam@email.com", u.getEmail());
     }
 
+    /**
+     * Metoda testGetKrestniJmeno kontroluje, že vrací správně křestní jméno
+     */
     @Test
     public void testGetKrestniJmeno() {
-        assertEquals("John", u.getKrestniJmeno());
+        assertEquals("Adam", u.getKrestniJmeno());
     }
 
+    /**
+     * Metoda testGetPrijmeni kontroluje, že vrací správně příjmení
+     */
     @Test
     public void testGetPrijmeni() {
-        assertEquals("Doe", u.getPrijmeni());
+        assertEquals("Zátopek", u.getPrijmeni());
     }
 
+    /**
+     * Metoda testGetHeslo kontroluje, že vrací správně heslo (nešifrovaně)
+     */
     @Test
     public void testGetHeslo() {
-        assertEquals("password", u.getHeslo());
+        assertEquals("heslo", u.getHeslo());
     }
 
+    /**
+     * Metoda testSetHeslo kontroluje, že nastavuje správně heslo
+     */
     @Test
     public void testSetHeslo() {
-        u.setHeslo("newpassword");
-        assertEquals("newpassword", u.getHeslo());
+        u.setHeslo("noveheslo");
+        assertEquals("noveheslo", u.getHeslo());
     }
 
+    /**
+     * Metoda testSetEmail kontroluje, že nastavuje správně Email
+     */
     @Test
     public void testSetEmail() {
-        u.setEmail("newuser@email.com");
-        assertEquals("newuser@email.com", u.getEmail());
+        u.setEmail("adamnovy@email.com");
+        assertEquals("adamnovy@email.com", u.getEmail());
     }
 
+    /**
+     * Metoda testSetKrestniJmeno kontroluje, že nastavuje správně křestní jméno
+     */
     @Test
     public void testSetKrestniJmeno() {
-        u.setKrestniJmeno("Jane");
-        assertEquals("Jane", u.getKrestniJmeno());
+        u.setKrestniJmeno("Jana");
+        assertEquals("Jana", u.getKrestniJmeno());
     }
 
+    /**
+     * Metoda testSetPrijmeni kontroluje, že nastavuje správně příjmení
+     */
     @Test
     public void testSetPrijmeni() {
-        u.setPrijmeni("Smith");
-        assertEquals("Smith", u.getPrijmeni());
+        u.setPrijmeni("Zátopková");
+        assertEquals("Zátopková", u.getPrijmeni());
     }
 }
