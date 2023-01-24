391cc5aaec88b3aba99766b704128805bcf9140e
david19au <david19au@gmail.com>
2023-01-24 23:57:47 +0100

    komentáře test Chat

--- src/test/java/cz/vse/tymovanicko/logika/ChatTest.java
+++ src/test/java/cz/vse/tymovanicko/logika/ChatTest.java
@@ -6,16 +6,24 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+/**
+ * Testovací třída ChatLog je na otestování dané třídy
+ *
+ * @author ?
+ * @version 1.0.0
+ */
 public class ChatTest {
 
     Chat chat = new Chat();
 
-
+    /**
+     * Metoda zkouší přidání nové zprávy, avšak test failuje, protože metoda čte první zprávu co je v souboru, jinak by ale test prošel
+     * @throws IOException
+     */
     @Test
     public void pridatZpravuTest() throws IOException {
-        chat.pridatZpravu("Test message 1");
-        assertEquals("Test message 1", chat.getChatLog().zpravy.get(0));
+        chat.pridatZpravu("testova zprava 1");
+        assertEquals("testova zprava 1", chat.getChatLog().zpravy.get(0));
     }
 
 
